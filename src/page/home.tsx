366e4e758191c9e59456199b504f59c4a91a6ceb
Ornella B <borges.ornella@gmail.com>
2024-02-19 21:49:24 +0100

    save

--- src/page/home.tsx
+++ src/page/home.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./home.css";
 type HomeProps = {
-  children: React.ReactNode; //
+  children: React.ReactNode;
 };
 export default function Home({ children }: HomeProps) {
   return <main className="main">{children}</main>;
