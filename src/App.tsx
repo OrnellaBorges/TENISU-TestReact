c78488e9015c2c4e661ad313699575f9ebb50c94
Ornella B <borges.ornella@gmail.com>
2024-02-19 11:44:17 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.scss";
+import "./reset.css";
+import "./variables.css";
+
+function App() {
+  //const [isSublistOpen, setIsSublistOpen] = useState<boolean>(false);
+
+  return <div>My app</div>;
+}
+
+export default App;
